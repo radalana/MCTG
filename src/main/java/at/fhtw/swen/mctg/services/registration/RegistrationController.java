bfa37afbbb78fb5b7ad075410068cc866ebec46c
Svetlana <radalana415@gmail.com>
2024-11-01 00:46:34 +0100

    fix UserRepository.findByUsername

--- src/main/java/at/fhtw/swen/mctg/services/registration/RegistrationController.java
+++ src/main/java/at/fhtw/swen/mctg/services/registration/RegistrationController.java
@@ -6,7 +6,8 @@ import at.fhtw.swen.mctg.httpserver.http.HttpStatus;
 import at.fhtw.swen.mctg.httpserver.server.Request;
 import at.fhtw.swen.mctg.httpserver.server.Response;
 
-import at.fhtw.swen.mctg.persistence.dao.UserDao;
+import at.fhtw.swen.mctg.persistence.dao.UserRepository;
+import at.fhtw.swen.mctg.services.login.AuthenticationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 
@@ -14,18 +15,20 @@ import java.util.Map;
 
 //session
 public class RegistrationController extends Controller {
-    private final UserDao userDao;
-    public RegistrationController() {
-        this.userDao = new UserDao();
+    private final AuthenticationService authService;
+    public RegistrationController(AuthenticationService authService) {
+        this.authService = authService;
     }
-
-
     public Response signup(Request request) {
         try {
             Map<String, String> signupData = this.getObjectMapper().readValue(request.getBody(), new TypeReference<Map<String,String>>(){});
-            User newUser = createUser(signupData);
+            String username = signupData.get("username");
+            String password = signupData.get("password");
+            if (isUserExists(username)) {
+
+            }
             if (newUser != null) {
-                if (userDao.save(newUser)){
+                if (userRepository.save(newUser)){
                     return new Response(
                             HttpStatus.CREATED,
                             "{ \"message\": Registration successful!}"
@@ -50,7 +53,7 @@ public class RegistrationController extends Controller {
         String username = signUpData.get("Username");
         String password = signUpData.get("Password");
         if (isValid(username) && isValid(password)) {
-            if (!userDao.isUserExists(username)){
+            if (!userRepository.isUserExists(username)){
                 System.out.println("User with login: " + "doesn't exist");
                 return new User(username, password);
             }
