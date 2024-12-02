e398739fae72dd0c87078e7ce8ba5a4e25e5ecea
Svetlana <radalana415@gmail.com>
2024-12-02 11:02:31 +0100

    Update UserRepository: Automatically create an empty card stack when a user is created.

---
+++ src/main/java/at/fhtw/swen/mctg/persistence/dao/StackRepository.java
@@ -0,0 +1,33 @@
+package at.fhtw.swen.mctg.persistence.dao;
+
+import at.fhtw.swen.mctg.persistence.DataAccessException;
+import at.fhtw.swen.mctg.persistence.UnitOfWork;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Stack;
+
+public class StackRepository {
+    private final UnitOfWork unitOfWork;
+    public StackRepository(UnitOfWork unitOfWork) {
+        this.unitOfWork = unitOfWork;
+    }
+
+    public int create(String userId) {
+        String sql = "INSERT INTO stack (user_id) VALUES (?)";
+        try (PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(sql, true)) {
+            preparedStatement.setString(1, userId);
+            preparedStatement.executeUpdate();
+            ResultSet generatedKey = preparedStatement.getGeneratedKeys();
+            if (generatedKey.next()) {
+                return generatedKey.getInt(1);
+            }else {
+                throw new DataAccessException("Could not create stack for user " + userId);
+            }
+        }catch (SQLException e) {
+            throw new DataAccessException("Database INSERT operation failed during stack creation: " + e.getMessage());
+        }
+    }
+
+}
--- src/main/java/at/fhtw/swen/mctg/persistence/dao/UserRepository.java
+++ src/main/java/at/fhtw/swen/mctg/persistence/dao/UserRepository.java
@@ -36,15 +36,44 @@ public class UserRepository {
             throw  new DataAccessException(e);
         }
     }
-    public void save(User user) {
+    public User findUserByToken(String token) {
+        //check if token null
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("Token cannot be null or empty");
+        }
+        String sql = "SELECT * FROM users WHERE token = ?";
+        try(PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(sql)) {
+            preparedStatement.setString(1, token);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new User(
+                    resultSet.getString("username"),
+                    resultSet.getString("password"),
+                    resultSet.getString("token"),
+                    resultSet.getInt("coins"),
+                    resultSet.getInt() //TODO при создании юзера сразу создавть стэк
+                );
+            }
+            return null; //TODO exception???
+        }catch (SQLException e) {
+            throw  new DataAccessException(e);
+        }
+    }
+    public String save(User user) {
         String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-        try(PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(sql)){
+        try(PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(sql, true)){
             preparedStatement.setString(1, user.getLogin());
             preparedStatement.setString(2, user.getPassword());
             preparedStatement.executeUpdate();
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                return generatedKeys.getString(1);
+            } else {
+                throw new DataAccessException("Could not save user");
+            }
         }catch (SQLException e) {
             //System.err.println("SQLException in save(): " + e.getMessage());
-            throw new DataAccessException("Error in save()", e);
+            throw new DataAccessException("Database INSERT operation failed during package creation: " + e.getMessage());
         }
     }
 }
