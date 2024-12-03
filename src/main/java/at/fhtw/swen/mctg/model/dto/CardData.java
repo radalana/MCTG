6a9cee819209e5f3d5941e8713723701b8fd5c7c
Svetlana <radalana415@gmail.com>
2024-12-03 15:00:17 +0100

    update: CardData.java add new Constructor for Step to create object Card by db query

--- src/main/java/at/fhtw/swen/mctg/model/dto/CardData.java
+++ src/main/java/at/fhtw/swen/mctg/model/dto/CardData.java
@@ -1,12 +1,19 @@
 package at.fhtw.swen.mctg.model.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 public class CardData {
     private final String Id;
     private final String Name;
     private final double Damage;
+    private String Type;
+    private String SubType;
+    private String Element;
 
+
+    //Constructor für das Erstellen von CardData-Objekten aus HTTP-Anfragen
     @JsonCreator
     public CardData(@JsonProperty("Id") String id,
                     @JsonProperty("Name") String name,
@@ -16,15 +23,15 @@ public class CardData {
         Name = name;
         Damage = damage;
     }
-    public String getId() {
-        return Id;
-    }
 
-    public String getName() {
-        return Name;
-    }
-
-    public double getDamage() {
-        return Damage;
+    //Constructor für das Laden von CardData aus der Datenbank
+    //Dient als Zwischenschritt zur Erstellung von echten Card objecten
+    public CardData(String id, String name, String type, String subtype, Double damage, String element) {
+        Id = id;
+        Name = name;
+        Damage = damage;
+        Type = type;
+        SubType = subtype;
+        Element = element;
     }
 }
