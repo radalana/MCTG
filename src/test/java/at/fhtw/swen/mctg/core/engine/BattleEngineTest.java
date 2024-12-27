1311b5aea3e542e3ec3478604b2ab38c09efe90b
Svetlana <radalana415@gmail.com>
2024-12-27 17:07:56 +0100

    test: add 3 tests BattleEngine.java

---
+++ src/test/java/at/fhtw/swen/mctg/core/engine/BattleEngineTest.java
@@ -0,0 +1,127 @@
+package at.fhtw.swen.mctg.core.engine;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import at.fhtw.swen.mctg.core.cards.Element;
+import at.fhtw.swen.mctg.core.cards.monsters.Wizard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import at.fhtw.swen.mctg.model.*;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+
+@ExtendWith(MockitoExtension.class)
+class BattleEngineTest {
+    @InjectMocks
+    private BattleEngine battleEngine;
+    @Mock User user1;
+    @Mock Deck deckA;
+    @Mock User user2;
+    @Mock Deck deckB;
+    @Spy
+    private RoundEngine roundEngine = new RoundEngine(user1, user2);
+
+    @BeforeEach
+    void setUp() throws NoSuchFieldException, IllegalAccessException {
+        // Erstellung des BattleEngine-Objekts und Ersetzung von RoundEngine
+        battleEngine = new BattleEngine(user1, user2);
+        Field field = BattleEngine.class.getDeclaredField("roundEngine");
+        field.setAccessible(true);
+        field.set(battleEngine, roundEngine); // Ersetzen des echten Objekts durch einen Spy
+
+        when(user1.getDeck()).thenReturn(deckA);
+        when(user2.getDeck()).thenReturn(deckB);
+
+    }
+
+    @Test
+    @DisplayName("User1 wins all rounds and User2 deck becomes empty")
+    void startBattleTest() {
+        when(deckA.isEmpty())
+                .thenReturn(false) // 1-й вызов
+                .thenReturn(false)
+                .thenReturn(false)// 2-й вызов
+                .thenReturn(false)
+                .thenReturn(false);
+        when(deckA.isEmpty())
+                .thenReturn(false) // 1-й вызов
+                .thenReturn(false)
+                .thenReturn(false)// 2-й вызов
+                .thenReturn(false)
+                .thenReturn(true);
+
+        doReturn(new Round(user1, user2, new Wizard("1", "1", 1.0, Element.NORMAL)))
+                .doReturn(new Round(user1, user2, new Wizard("2", "2", 1.0, Element.NORMAL)))
+                .doReturn(new Round(user1, user2, new Wizard("3", "3", 2.0, Element.NORMAL)))
+                .doReturn(new Round(user1, user2, new Wizard("4", "4", 3.0, Element.NORMAL)))
+                .doReturn(new Round(user1, user2, new Wizard("4", "4", 3.0, Element.NORMAL)))
+                .when(roundEngine).run();
+
+        Battle battle = battleEngine.startBattle();
+        assertEquals(4, battle.getRounds().size());
+        assertEquals(4, battle.getUser1BattleResult().getResult());
+        assertEquals(0, battle.getUser2BattleResult().getResult());
+    }
+
+
+    @Test
+    @DisplayName("User1 gewinnt 5 mal, User2 gewinnt 1 mal, 1 mal unentschieden")
+    void startBattleTest_1_round_from_4_is_draw(){
+        when(deckA.isEmpty())
+                .thenReturn(false) //Before dem 1. round
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false);
+        when(deckA.isEmpty())
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                .thenReturn(false)
+                        .thenReturn(true);
+
+        doReturn(new Round(user1, user2, new Wizard("1", "1", 1.0, Element.NORMAL)))// nach dem 1.Round: user1 - 5 Karten | user2 - 3 Karten
+                .doReturn(new Round(user1, user2, new Wizard("2", "2", 1.0, Element.NORMAL)))// nach dem 2.Round: user1 - 6 Karten | user2 - 2 Karten
+                .doReturn(new Round(user1, user2, new Wizard("3", "3", 2.0, Element.NORMAL)))// nach dem 3.Round: user1 - 7 Karten | user2 - 1 Karten
+                .doReturn(new Round(user2, user1, new Wizard("4", "4", 3.0, Element.NORMAL)))// nach dem 4.Round: user1 - 6 Karten | user2 - 2 Karten
+                .doReturn(new Round(null, null, null))//nach 5. Round:  dem user1 - 6 Karten | user2 - 2 Karten
+                .doReturn(new Round(user1, user2, new Wizard("4", "4", 4.0, Element.NORMAL)))// nach 6.Round: user1 - 7 Karten | user2 - 1 Karten
+                .doReturn(new Round(user1, user2, new Wizard("4", "4", 4.0, Element.NORMAL)))// nach 7.Round: user1 - 8 Karten | user2 - 0 Karten
+                .when(roundEngine).run();
+
+        Battle battle = battleEngine.startBattle();
+        assertEquals(7, battle.getRounds().size());
+        assertEquals(5, battle.getUser1BattleResult().getResult());
+        assertEquals(1, battle.getUser2BattleResult().getResult());
+    }
+
+    @DisplayName("Alle Rounds unentschieden, Battle endet nach 100 Rounds")
+    @Test
+    void startBattle_max_100_rounds() {
+        when(deckA.isEmpty())
+                .thenReturn(false);
+        when(deckB.isEmpty())
+                .thenReturn(false);
+
+        doReturn(new Round(null, null, null)).when(roundEngine).run();
+
+        Battle battle = battleEngine.startBattle();
+        assertEquals(100, battle.getNumberOfRounds());
+    }
+
+}
