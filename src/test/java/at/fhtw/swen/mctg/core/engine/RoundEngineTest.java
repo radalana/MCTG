29163b54d1f520030c1d2adf5cdc212911517bce
Svetlana <radalana415@gmail.com>
2024-12-27 14:54:18 +0100

    test: add 2 tests for RoundEngine.java

---
+++ src/test/java/at/fhtw/swen/mctg/core/engine/RoundEngineTest.java
@@ -0,0 +1,78 @@
+package at.fhtw.swen.mctg.core.engine;
+
+import at.fhtw.swen.mctg.core.cards.Element;
+import at.fhtw.swen.mctg.core.cards.Monster;
+import at.fhtw.swen.mctg.core.cards.monsters.Ork;
+import at.fhtw.swen.mctg.model.Card;
+import at.fhtw.swen.mctg.model.Deck;
+import at.fhtw.swen.mctg.model.Round;
+import at.fhtw.swen.mctg.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RoundEngineTest {
+    @InjectMocks
+    private RoundEngine roundEngine;
+    @Mock
+    private User user1;
+    @Mock
+    private User user2;
+    @Mock
+    Deck deckA;
+    @Mock
+    Deck deckB;
+
+
+    @BeforeEach
+    void setUp() {
+        roundEngine = new RoundEngine(user1, user2);
+        when(user1.getDeck()).thenReturn(deckA);
+        when(user2.getDeck()).thenReturn(deckB);
+    }
+    @Test
+    @DisplayName("Bestimmt korrekt Gewinner und Verlierer basierend auf der Kartenstärke" +
+            " und gibt ein gültiges Round-Objekt zurück.")
+    void runTest() {
+        Card cardA = new Ork("ork1", "Ork1", 100.0, Element.NORMAL);
+        Card cardB = new Ork("ork2", "Ork2", 5.0, Element.NORMAL);
+
+        when(deckA.getRandomCard()).thenReturn(cardA);
+        when(deckB.getRandomCard()).thenReturn(cardB);
+        Round round = roundEngine.run();
+        assertNotNull(round);
+        assertEquals(user1, round.getWinner());
+        assertEquals(user2, round.getLooser());
+        assertEquals(cardB, round.getDefeatedCard());
+
+        verify(deckA).addCard(cardB);
+        verify(deckB).removeCard(cardB);
+    }
+
+    @Test
+    @DisplayName("Unentschieden – keine Kartenbewegung")
+    void runTest_Draw() {
+        Card cardA = new Ork("ork1", "Ork1", 1.0, Element.NORMAL);
+        Card cardB = new Ork("ork2", "Ork2", 1.0, Element.NORMAL);
+
+        when(deckA.getRandomCard()).thenReturn(cardA);
+        when(deckB.getRandomCard()).thenReturn(cardB);
+
+        Round round = roundEngine.run();
+        assertNotNull(round);
+        assertNull(round.getWinner());
+        assertNull(round.getLooser());
+        assertNull(round.getDefeatedCard());
+
+        verify(deckA, never()).addCard(cardB);
+        verify(deckB, never()).removeCard(cardB);
+    }
+}
